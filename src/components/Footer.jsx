3d14a5401abf4680888c37fc4f145859dba4713e
Trung <mrtrunq@gmail.com>
2025-06-17 23:29:39 +0700

    feat: update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react";
 export const Footer = () => {
     return (
         <footer className="py-12 px-4 bg-card relative border-t border-border pt-10 flex flex-wrap justify-between items-center">
-            <p className="text-sm text-muted-foreground ">&copy; Trung {new Date().getFullYear()}</p>
+            <p className="text-sm text-muted-foreground ">&copy; {new Date().getFullYear()} Trung Hoang. All rights reserved</p>
             <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
                 <ArrowUp size={20} />
             </a>
